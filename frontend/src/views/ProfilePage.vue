e7e151f0fcbde2ff34b1d2742350a46debeea38c
antotmp <antomaric.pub@gmail.com>
2022-07-05 13:07:53 +0200

    getUserevents impl.

--- frontend/src/views/ProfilePage.vue
+++ frontend/src/views/ProfilePage.vue
@@ -1,7 +1,6 @@
 <template>
   <ion-page>
-
-    <ion-header>
+    <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Profil</ion-title>
       </ion-toolbar>
@@ -13,70 +12,80 @@
           <ion-title size="large">Profil</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <!-- Profil Page Layout -->
+      <!-- Profilbild -->
+      <ion-img style="width: 100px; height: 100px;" src="assets/img/ppic.png"></ion-img>
+
+      <!-- User Information (ev. Name auslesen aus DB) -->
+      <ion-item>
+        <p>Vorname</p>
+        <p>Nachname</p>
+      </ion-item>
+
+      <!-- Event Titel mit Badge -->
+      <ion-item>
+        <ion-label><b>Bevorstehende Events</b></ion-label>
+        <ion-badge slot="end">Anzahl eingetragener Events: {{ userveranstaltung.value }}</ion-badge>
+      </ion-item>
+
+      <!-- Liste der Events -->
+      <ion-list>
+        <ion-item>
+          <ion-grid>
+            <!-- Beschreibung der Listenelemente-->
+            <ion-row>
+              <ion-col><b>Sportart</b></ion-col>
+              <ion-col><b>Beschreibung</b></ion-col>
+              <ion-col><b>Wo</b></ion-col>
+              <ion-col><b>Wann</b></ion-col>
+            </ion-row>
+          </ion-grid>
+        </ion-item>
+        <!-- Alle eingetragenen Events als Liste-->
+        <!-- Router link noch einfügen: :router-link="'/tabs/tasks/' + task._id" bsp. von task-list-->
+        <ion-item button v-bind:key="uevent" v-for="uevent in userveranstaltung">
+          <ion-label>{{ uevent.activity }}</ion-label>
+          <ion-label>{{ uevent.beschreibung }}</ion-label>
+          <ion-label>{{ uevent.ort }}</ion-label>
+          <ion-label>{{ uevent.datum }}</ion-label>
+        </ion-item>
+      </ion-list>
     </ion-content>
-
-  <ion-grid>
-
-    <ion-row>
-      <ion-col>
-          <ion-list>
-    <ion-item v-for="item in items" :key="item.src">
-      <ion-thumbnail slot="start">
-        <ion-img :src="item.src"></ion-img>
-      </ion-thumbnail>
-      <ion-label>{{item.text}}</ion-label>
-    </ion-item>
-  </ion-list>
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col>
-        Name Vorname
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col>
-        <ion-list-header>
-          <ion-label>Bevorstehende Events</ion-label>
-        </ion-list-header>
-      </ion-col>
-    </ion-row>
-
-    <ion-row>
-      <ion-col>
-        <ion-list>
-          <ion-item>
-            <ion-label>Event Eintrag 1</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>Event Eintrag 2</ion-label>
-          </ion-item>
-          <ion-item>
-            <ion-label>Event Eintrag 3</ion-label>
-          </ion-item>
-        </ion-list>
-      </ion-col>
-    </ion-row>
-
-  </ion-grid>
-
   </ion-page>
 </template>
 
-<script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonListHeader, IonList, IonCol, IonGrid, IonRow,IonImg, IonItem, IonLabel, IonThumbnail } from '@ionic/vue';
+<script lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonImg,
+  IonLabel,
+  IonBadge,
+  IonItem
+} from '@ionic/vue';
+import { getUserevents } from '@/composables/getUserevents';
 
 export default {
   name: 'Profile',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonListHeader, IonList, IonCol, IonGrid, IonRow, IonImg, IonItem, IonLabel, IonThumbnail },
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonImg,
+    IonLabel,
+    IonBadge,
+    IonItem
+  },
   setup() {
-    const items = [{
-      'text': 'Item 1',
-      'src': '../public/assets/img/ppic.jpg'
-    }];
-    return { items }
-  }
-}
+    const { userveranstaltung } = getUserevents();
+
+    return { userveranstaltung }
+  },
+};
 </script>
